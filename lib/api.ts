3580871aecb5bbc2bf8597ec8b03b14dc72b1f46
Ayub <ayub@localhost.localdomain>
2023-08-14 16:55:33 -0400

    first init but the app is finished

---
+++ lib/api.ts
@@ -0,0 +1,39 @@
+const fetcher = async ({ url, method, body, json = true }) => {
+  const res = await fetch(url, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  if (json) {
+    const data = await res.json();
+    return data;
+  }
+};
+
+export const register = async (user) => {
+  return fetcher({
+    url: "/api/register",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
+export const signin = async (user) => {
+  return fetcher({
+    url: "/api/signin",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
+
+export const createNewProject = async (name) => {
+  return fetcher({
+    url: "/api/project",
+    method: "POST",
+    body: { name },
+  });
+};
